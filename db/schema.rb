a3ed6a8695cecc0c02af9f918f6eb5de8e9da585
Joyce San Pascual <jjoyce.sanpascual@gmail.com>
2015-05-22 18:37:10 +0800

    model and database changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150522095035) do
+ActiveRecord::Schema.define(version: 20150522103211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,16 @@ ActiveRecord::Schema.define(version: 20150522095035) do
 
   add_index "messages", ["user_id"], name: "index_messages_on_user_id", using: :btree
 
+  create_table "qualities", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "count"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "qualities", ["user_id"], name: "index_qualities_on_user_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
@@ -34,4 +44,5 @@ ActiveRecord::Schema.define(version: 20150522095035) do
   end
 
   add_foreign_key "messages", "users"
+  add_foreign_key "qualities", "users"
 end
